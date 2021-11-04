c33a529eafa5b0fca59325dfcfc42c7665b2c45c
rami mustonen <t0mura00@students.oamk.fi>
2021-11-04 12:23:29 +0200

    koodin pohja

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div>
+        <div>
+            head
+            </div>
+            <div>
+              body
+            </div>
+    </div>
+    
+  );
+}
+
+export default App;
