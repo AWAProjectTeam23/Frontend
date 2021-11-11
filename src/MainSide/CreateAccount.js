d6d490157b9223e73240be4669e650f56686dd97
Johanna Soini <johanna.k.soini@gmail.com>
2021-11-11 15:22:56 +0200

    changes in createaccount

--- src/MainSide/CreateAccount.js
+++ src/MainSide/CreateAccount.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import styles from './CreateAccount.module.css';
 
-export default function CreateAccount() {
+
+export default function CreateAccount(props) {
+
+        
     return (
         <div>
-            CreateAccount
+            
+        <div className= { styles.container }>
+
+        <span className={ styles.header }>{ props.topic }Create account</span> { props.body }
+        <div>
+<span className={ styles.much}>{ props.topic }Account name</span> { props.body }
+<div>
+
+<span className={ styles.much}>{ props.topic } Password </span> { props.body }
+</div>
+</div>
+        </div>
         </div>
     )
 }
+
