b9c99a50060d5ecda2ed6c0db1243813b5d08350
Johanna Soini <johanna.k.soini@gmail.com>
2021-11-30 09:48:20 +0200

    BarLayout done

--- src/ManagerSide/Managerbar.js
+++ src/ManagerSide/Managerbar.js
@@ -5,9 +5,11 @@ import { Link } from 'react-router-dom'
 export default function Managerbar(props) {
     return (
         <div className={style.ManagerBar}>
-            <div className={style.Logo_margin}>
-                <Link to="/"><div style={{color:"white"}}>LOGO</div></Link>
-            </div>
+             <div className={style.Logo_margin}>
+                    <Link to="/"><div style={{color:"white"}}><img src={`/images/logo.png`} 
+                    height={50}
+                    width={190}/></div></Link>
+                </div>
             <div className={style.select_margin}>
                 <select className={style.select_size} onChange={props.selectChange}>
                     {props.selectValue.map((option)=>(<option value={option.Id}>{option.name}</option>))}
